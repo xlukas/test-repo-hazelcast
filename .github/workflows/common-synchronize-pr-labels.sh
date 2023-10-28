4050519b51723c08294b11665a4598b4dcc6ccf7
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2023-10-28 17:41:56 +0200

    Improving logging
    GitOrigin-RevId: 3475b29d67107ac55e1f0a530098a3907ed0bc64

--- .github/workflows/common-synchronize-pr-labels.sh
+++ .github/workflows/common-synchronize-pr-labels.sh
@@ -29,6 +29,8 @@ LABELS_TO_ADD=$(comm -23 <(echo "$SOURCE_LABELS") <(echo "$TARGET_LABELS") | joi
 LABELS_TO_REMOVE=$(comm -13 <(echo "$SOURCE_LABELS") <(echo "$TARGET_LABELS") | join_with_comma)
 
 if [ -n "$LABELS_TO_ADD" ] || [ -n "$LABELS_TO_REMOVE" ]; then
+  echo "Adding labels: $LABELS_TO_ADD"
+  echo "Removing labels: $LABELS_TO_REMOVE"
   gh pr edit --repo $TARGET_REPO $TARGET_PR --add-label "$LABELS_TO_ADD" --remove-label "$LABELS_TO_REMOVE"
   echo "Labels synchronized from PR $SOURCE_PR to PR $TARGET_PR"
 else
